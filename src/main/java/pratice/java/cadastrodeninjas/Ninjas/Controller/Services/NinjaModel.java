671fe5342d3c0a57a1412c65e6020c07784b075f
Maike Silva <maikesama@gmail.com>
2026-04-28 21:57:26 -0300

    feat:coloca o Lombok na classe para abstracao

--- src/main/java/pratice/java/cadastrodeninjas/Ninjas/Controller/Services/NinjaModel.java
+++ src/main/java/pratice/java/cadastrodeninjas/Ninjas/Controller/Services/NinjaModel.java
@@ -1,9 +1,16 @@
 package pratice.java.cadastrodeninjas.Ninjas.Controller.Services;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pratice.java.cadastrodeninjas.Missoes.MissoesModel;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -22,27 +29,5 @@ public class NinjaModel {
     @JoinColumn (name = "missoes_id") //Foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {}
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-}
+
+as1z
